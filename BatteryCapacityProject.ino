2a6f0d87cc1f4c0e7d9677b2dad9831e9302c221
ezza2000 <erik@redpulse.ccom.au>
2024-05-24 10:42:38 +1000

    Almost done

--- BatteryCapacityProject.ino
+++ BatteryCapacityProject.ino
@@ -35,16 +35,17 @@ String queryString = "?value1=57&value2=25";
 bool requestMade = false;
 
 unsigned long startMillis = 0;
+unsigned long displayHours = 0;
 unsigned long displayMinutes = 0;
 unsigned long displaySeconds = 0;
 
 unsigned long previousMillis = 0; // Store the last time the voltage was updated
-const long interval = 20000; // Interval at which to update ThingSpeak (20 secs)
+const long interval = 120000; // Interval at which to update ThingSpeak (2 minutes)
 unsigned long previousDisplayMillis = 0; // Store the last time the display was updated
 const long displayInterval = 1000; // Interval at which to update the display (1 second)
 
 bool buttonPressed = false;
-float valPot; // Variable to store the analog value read
+float valBatt; // Variable to store the analog value read
 float mAh = 0.0;
 float batteryVoltage = 0.0;
 float shuntRes = 2.5; // In Ohms – Shunt resistor resistance
@@ -77,11 +78,11 @@ void setup()
   display.display();
   delay(2000); // Pause for 2 seconds
   display.clearDisplay();
-  batteryVoltage = readVoltage(); 
+  batteryVoltage = calcVoltage(); 
   startingVoltage = batteryVoltage;
   
   // Display the initial message
-  helloLcd();
+  startupLcd();
 
   // Clear ThingSpeak channel data before starting a new test
   clearThingSpeakChannel();
@@ -173,30 +174,42 @@ void elapasedTime()
   unsigned long elapsedMillis = currentMillis - startMillis;
   unsigned long elapsedSeconds = elapsedMillis / 1000;
   unsigned long elapsedMinutes = elapsedSeconds / 60;
+  unsigned long elapsedHours = elapsedMinutes / 60;
+  displayHours = elapsedHours % 60;
   displayMinutes = elapsedMinutes % 60;
   displaySeconds = elapsedSeconds % 60;
 }
 
-float readVoltage() 
+float calcVoltage() 
 {
-  float dividVolt = readPotent();
-  float actualVolt = dividVolt * 2;
-  return actualVolt;
+  float totalVoltage = 0.0;
+  const int numReadings = 4;
+
+  for (int i = 0; i < numReadings; i++) 
+  {
+    float dividVolt = readBattVolt();
+    float actualVolt = dividVolt * 2;
+    totalVoltage += actualVolt;
+    delay(10); // Small delay between readings to stabilize
+  }
+
+  float averageVoltage = totalVoltage / numReadings;
+  return averageVoltage;
 }
 
-float readCurrent()
+float calcCurrent()
 {
-  float batteryVolt = readVoltage(); // Get the battery voltage
+  float batteryVolt = calcVoltage(); // Get the battery voltage
   shuntRes = 2.5; // Load resistor in ohms
   float current = batteryVolt / shuntRes; // Calculate current using Ohm's Law
   return current;
 }
 
-float readPotent() 
+float readBattVolt() 
 {
-  valPot = analogRead(A1);
+  valBatt = analogRead(A1);
   Serial.print("The digital value is: ");
-  float dividVolt = valPot / 313.0; // Ensure float division
+  float dividVolt = valBatt / 313.0; // Ensure float division
   Serial.println(dividVolt);
   return dividVolt;
 }
@@ -216,7 +229,7 @@ void updateThingSpeak()
     Serial.println("\nConnected.");
   }
 
-  batteryVoltage = readVoltage();
+  batteryVoltage = calcVoltage();
   ThingSpeak.setField(1, batteryVoltage);
   ThingSpeak.setStatus(myStatus);
   
@@ -237,6 +250,8 @@ void lcdDisplay()
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
   display.print("Time: ");
+  display.print(displayHours);
+  display.print("h ");
   display.print(displayMinutes);
   display.print("m ");
   display.print(displaySeconds);
@@ -262,7 +277,7 @@ void lcdDisplay()
   display.display();
 }
 
-void goodbyeLcd(unsigned long minutes, unsigned long seconds)
+void finishLcd(unsigned long hours, unsigned long minutes, unsigned long seconds)
 {
   display.clearDisplay();
   display.setTextSize(1);
@@ -271,23 +286,26 @@ void goodbyeLcd(unsigned long minutes, unsigned long seconds)
   display.print("Test Complete");
   display.setCursor(0, 10);
   display.print("Elapsed Time: ");
+  display.setCursor(0, 20);
+  display.print(displayHours);
+  display.print("h ");
   display.print(minutes);
   display.print("m ");
   display.print(seconds);
   display.print("s");
-  display.setCursor(0, 20);
+  display.setCursor(0, 30);
   display.print("Capacity: ");
   display.print(mAh);
   display.print(" mAh");
-  display.setCursor(0, 30);
-  display.print("Starting Voltage: ");
   display.setCursor(0, 40);
-  display.print(startingVoltage);
+  display.print("Voltage: ");
+  display.setCursor(0, 50);
+  display.print(batteryVoltage);
   display.print("V");
   display.display();
 }
 
-void helloLcd()
+void startupLcd()
 {
   display.clearDisplay();
   display.setTextSize(1);
@@ -315,29 +333,39 @@ void relayOff()
 
 void loop() 
 {
-  if (!buttonPressed && digitalRead(BUTTON_PIN) == LOW) 
-  {
-    Serial.println("Button pressed");
-    buttonPressed = true;
-    delay(1000);
-    startMillis = millis();
-    updateThingSpeak();
-    relayOn();
-  }
-  
-  if (buttonPressed)
-  {
-    unsigned long currentMillis = millis();
+  unsigned long currentMillis = millis();
 
-    if (currentMillis - previousDisplayMillis >= displayInterval)
+  if (!buttonPressed) 
+  {
+    // Continuously read and display battery voltage before button is pressed
+    if (currentMillis - previousDisplayMillis >= displayInterval) 
     {
-      batteryVoltage = readVoltage(); // Update voltage before displaying
-      current = readCurrent(); // Implement readCurrent() to read current value
-      mAh += (current / 3.6); // Update mAh based on current and time interval
       previousDisplayMillis = currentMillis;
+      batteryVoltage = calcVoltage(); // Update voltage
+      startupLcd();
+    }
+
+    if (digitalRead(BUTTON_PIN) == LOW) 
+    {
+      Serial.println("Button pressed");
+      buttonPressed = true;
+      delay(1000);
+      startMillis = millis();
+      updateThingSpeak();
+      relayOn();
+    }
+  }
+  else 
+  {
+    // Main loop after button is pressed
+    if (currentMillis - previousDisplayMillis >= displayInterval) 
+    {
+      previousDisplayMillis = currentMillis;
+      batteryVoltage = calcVoltage(); // Update voltage before displaying
+      current = calcCurrent(); // Implement readCurrent() to read current value
+      mAh += (current / 3.6); // Update mAh based on current and time interval
       elapasedTime();
       lcdDisplay();
-
     }
 
     if (currentMillis - previousMillis >= interval) 
@@ -350,9 +378,15 @@ void loop()
     {
       makeRequest(PATH_NAME, queryString);
       requestMade = true;
-      goodbyeLcd(displayMinutes, displaySeconds);
+      finishLcd(displayHours, displayMinutes, displaySeconds);
       relayOff();
-      while (true); // Stop the loop
+      while (true) 
+      {
+        // Keep reading and displaying voltage after test is complete
+        batteryVoltage = calcVoltage();
+        lcdDisplay(); // Update the display with current voltage
+        delay(displayInterval); // Maintain display update interval
+      }
     }
   }
 }
